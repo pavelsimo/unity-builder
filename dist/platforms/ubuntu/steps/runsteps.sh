ac8a565998862b2a2ca794352d4c582b708cd39f
Simo Pavel (BD/XDC-DTC) <sip4be@bosch.com>
2024-06-28 17:13:14 +0200

    :alembic: trying something out

--- dist/platforms/ubuntu/steps/runsteps.sh
+++ dist/platforms/ubuntu/steps/runsteps.sh
@@ -10,7 +10,7 @@ if [ "$SKIP_ACTIVATION" != "true" ]; then
 
   echo $HTTP_PROXY
   echo $HTTPS_PROXY
-  export NO_PROXY=10.224.197.250,$NO_PROXY
+  export NO_PROXY=10.224.197.250,.unity3d.com,$NO_PROXY
   export NODE_TLS_REJECT_UNAUTHORIZED=0
   echo $NO_PROXY
 
