1635b417de569b5f8571dc0e3310adfd2097a4e2
Simo Pavel (BD/XDC-DTC) <sip4be@bosch.com>
2024-07-01 08:36:17 +0200

    :alembic: trying something out

--- dist/platforms/ubuntu/steps/runsteps.sh
+++ dist/platforms/ubuntu/steps/runsteps.sh
@@ -12,8 +12,8 @@ if [ "$SKIP_ACTIVATION" != "true" ]; then
   echo "#          BDC - Environment        #"
   echo "#####################################"
 
-  export no_proxy=10.224.197.250,.unity3d.com,.unity.com,.voltstro.dev,$no_proxy
-  export NO_PROXY=10.224.197.250,.unity3d.com,.unity.com,.voltstro.dev,$NO_PROXY
+  export no_proxy=10.224.197.250,.unity3d.com,.unity.com,.bintray.com,104.21.15.144,34.36.199.114,172.67.162.200,.voltstro.dev,$no_proxy
+  export NO_PROXY=10.224.197.250,.unity3d.com,.unity.com,.bintray.com,104.21.15.144,34.36.199.114,172.67.162.200,.voltstro.dev,$NO_PROXY
   export NODE_TLS_REJECT_UNAUTHORIZED=0
 
   printenv
